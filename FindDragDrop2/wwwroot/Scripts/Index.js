6bd163501ef86178dc835a895fc65e3d76469f1f
aaalic <frida.ost2@gmail.com>
2017-12-07 12:10:44 +0100

    JS och CSS

---
+++ FindDragDrop2/wwwroot/Scripts/Index.js
@@ -0,0 +1,65 @@
+﻿$(document).ready(function () {
+
+    $.ajax({
+        url: "/Home/GetObjects",
+        type: "GET",
+        success: function (result) {
+            $("#myPartialContainer").html(result);
+                
+                success: $(function () {
+                    $("#dragFirst").draggable({
+                        scope: "first"
+                    });
+
+                    $("#dropFirst").droppable({
+                        scope: "first",
+                        drop: function (event, ui) {
+                            $(this).css("background-color", "SeaGreen")
+                        },
+                        out: function (event, ui) {
+                            $(this).css("background-color", "")
+                        }
+                    });
+                });
+        }
+
+    });
+
+
+});
+
+            
+
+            //    $("#dragSecond").draggable({
+            //        scope: "second"
+            //    });
+
+                //$("#dropSecond").droppable({
+                //    scope: "second",
+                //    drop: function (event, ui) {
+                //        $(this).css("background-color", "SeaGreen")
+                //    },
+                //    out: function (event, ui) {
+                //        $(this).css("background-color", "")
+                //    }
+                //});
+
+                //$("#dragThird").draggable({
+                //    scope: "third"
+                //});
+
+                //$("#dropThird").droppable({
+                //    scope: "third",
+                //    drop: function (event, ui) {
+                //        $(this).css("background-color", "SeaGreen")
+                //    },
+                //    out: function (event, ui) {
+                //        $(this).css("background-color", "")
+                //    }
+                //});
+
+          
+
+
+
+
