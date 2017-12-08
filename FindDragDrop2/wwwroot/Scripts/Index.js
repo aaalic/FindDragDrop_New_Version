2a45a0ba41faead1d0dc83804e9d55f284f4284f
aaalic <frida.ost2@gmail.com>
2017-12-08 09:44:20 +0100

    Alla objekt inne i rummet

--- FindDragDrop2/wwwroot/Scripts/Index.js
+++ FindDragDrop2/wwwroot/Scripts/Index.js
@@ -20,7 +20,120 @@
                             $(this).css("background-color", "")
                         }
                     });
+
+
+                $("#dragSecond").draggable({
+                    scope: "second"
                 });
+
+                $("#dropSecond").droppable({
+                    scope: "second",
+                    drop: function (event, ui) {
+                        $(this).css("background-color", "SeaGreen")
+                    },
+                    out: function (event, ui) {
+                        $(this).css("background-color", "")
+                    }
+                });
+
+                $("#dragThird").draggable({
+                    scope: "third"
+                });
+
+                $("#dropThird").droppable({
+                    scope: "third",
+                    drop: function (event, ui) {
+                        $(this).css("background-color", "SeaGreen")
+                    },
+                    out: function (event, ui) {
+                        $(this).css("background-color", "")
+                    }
+                });
+
+                $("#dragFourth").draggable({
+                    scope: "fourth"
+                });
+
+                $("#dropFourth").droppable({
+                    scope: "fourth",
+                    drop: function (event, ui) {
+                        $(this).css("background-color", "SeaGreen")
+                    },
+                    out: function (event, ui) {
+                        $(this).css("background-color", "")
+                    }
+                });
+
+                $("#dragFifth").draggable({
+                    scope: "fifth"
+                });
+
+                $("#dropFifth").droppable({
+                    scope: "fifth",
+                    drop: function (event, ui) {
+                        $(this).css("background-color", "SeaGreen")
+                    },
+                    out: function (event, ui) {
+                        $(this).css("background-color", "")
+                    }
+                });
+
+                $("#dragSixth").draggable({
+                    scope: "sixth"
+                });
+
+                $("#dropSixth").droppable({
+                    scope: "sixth",
+                    drop: function (event, ui) {
+                        $(this).css("background-color", "SeaGreen")
+                    },
+                    out: function (event, ui) {
+                        $(this).css("background-color", "")
+                    }
+                });
+
+                $("#dragSeventh").draggable({
+                    scope: "seventh"
+                });
+
+                $("#dropSeventh").droppable({
+                    scope: "seventh",
+                    drop: function (event, ui) {
+                        $(this).css("background-color", "SeaGreen")
+                    },
+                    out: function (event, ui) {
+                        $(this).css("background-color", "")
+                    }
+                });
+
+                $("#dragEigth").draggable({
+                    scope: "eigth"
+                });
+
+                $("#dropEigth").droppable({
+                    scope: "eigth",
+                    drop: function (event, ui) {
+                        $(this).css("background-color", "SeaGreen")
+                    },
+                    out: function (event, ui) {
+                        $(this).css("background-color", "")
+                    }
+                });
+
+                $("#dragNinth").draggable({
+                    scope: "ninth"
+                });
+
+                $("#dropNinth").droppable({
+                    scope: "ninth",
+                    drop: function (event, ui) {
+                        $(this).css("background-color", "SeaGreen")
+                    },
+                    out: function (event, ui) {
+                        $(this).css("background-color", "")
+                    }
+                });
+            });
         }
 
     });
@@ -30,33 +143,6 @@
 
             
 
-            //    $("#dragSecond").draggable({
-            //        scope: "second"
-            //    });
-
-                //$("#dropSecond").droppable({
-                //    scope: "second",
-                //    drop: function (event, ui) {
-                //        $(this).css("background-color", "SeaGreen")
-                //    },
-                //    out: function (event, ui) {
-                //        $(this).css("background-color", "")
-                //    }
-                //});
-
-                //$("#dragThird").draggable({
-                //    scope: "third"
-                //});
-
-                //$("#dropThird").droppable({
-                //    scope: "third",
-                //    drop: function (event, ui) {
-                //        $(this).css("background-color", "SeaGreen")
-                //    },
-                //    out: function (event, ui) {
-                //        $(this).css("background-color", "")
-                //    }
-                //});
 
           
 
